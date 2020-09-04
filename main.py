c22001b0a5e2fae37de7617a82f33ffd9122a8b6
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2020-09-04 13:12:12 -0400

    edited represntation of the disk space

--- main.py
+++ main.py
@@ -13,11 +13,18 @@ def main():
     """
     disks = disk_space()
     for disk in disks:
-        final = f"{stylize(disk, fg(197), attr('bold'))}" \
-            + f"\n{stylize(print_stats(disks[disk]), attr('res_blink'))}" \
-            + f"\n{print_graph(disks[disk])[0]}   " \
-            + f"{stylize(print_graph(disks[disk])[1], attr('dim'))}\n"
-        print(final)
+        print(f"{stylize(disk, fg(1), attr('bold'))}")
+        print(f"{stylize(print_stats(disks[disk]), attr('res_blink'))}")
+        usage = round(usage_percent(disks[disk]), 2)
+        if usage >= 85:
+            usage = str(usage) + '% full'
+            print(f"{print_graph(disks[disk])}   {stylize(usage, fg(197), attr('blink'))}")
+        elif usage >= 60:
+            usage = str(usage) + '% full'
+            print(f"{print_graph(disks[disk])}   {stylize(usage, fg(214), attr('dim'))}")
+        else:
+            usage = str(usage) + '% full'
+            print(f"{print_graph(disks[disk])}   {stylize(usage, fg(82), attr('dim'))}\n")
 
 def disk_space() -> dict:
     disks = {}
@@ -31,14 +38,16 @@ def disk_space() -> dict:
         # disk[1] is Path to the disk partition,
         # if it starts with media will grab it
         if 'media' in disk[1]:
-            disks[disk[1].split('/')[-1]] = {"total": psutil.disk_usage(disk[1]).total,
-                            "used": psutil.disk_usage(disk[1]).used,
-                            "free": psutil.disk_usage(disk[1]).free}
+            try:
+                disks[disk[1].split('/')[-1]] = {"total": psutil.disk_usage(disk[1]).total,
+                                                "used": psutil.disk_usage(disk[1]).used,
+                                                "free": psutil.disk_usage(disk[1]).free}
+            except:
+                continue
     return disks
 
-
 # Function to convert bytes into Gb or Mb based on its size
-def bytes_to_readable_format(bytes: int) -> int:
+def bytes_to_readable_format(bytes: int) -> str:
     gb = round(bytes / 1024 / 1024 / 1024, 2)
     if gb < 1.0:
         return f'{gb * 1024} MB'
@@ -51,17 +60,23 @@ def print_graph(disk: dict) -> str:
     for bar and for empty space.
     """
     used = 0
-    bar = ''
+    bar = ' '
     try:
         used = int((disk['used'] / disk['total']) * 36)
     except:
         raise ValueError("Expected total, used, and free as dict keys")
     for i in range(1, used + 1):
         bar += '█'
+    bar += '▒'
     for i in range(1, 37 - used):
         bar += '░'
-    free_portion = round(disk['used'] / disk['total'] * 100, 2)
-    return f'{bar}', f'{free_portion} %'
+    return bar
+
+def usage_percent(disk: dict) -> dict:
+    try:
+        return disk['used'] / disk['total'] * 100
+    except:
+        raise ValueError("Expected total, used, and free as dict keys")
 
 def print_stats(disk: dict) -> str:
     try:
@@ -73,7 +88,13 @@ def print_stats(disk: dict) -> str:
     return f'Total: {total}   Used: {used}   Free: {free}'
 
 
-# "████████████████░░░░░░░░░"
+# from gi.repository import Gtk
+# gset = Gtk.Settings.get_default ()
+# themename = gset.get_property ("gtk-theme-name")
+# cprov = Gtk.CssProvider.get_named (themename)
+# print (cprov.to_string())
+
+# "████████████████▒░░░░░░░░"
 # print('\n', psutil.sensors_temperatures())
 # print('\n', psutil.Process(5622).memory_info())
 
