f79cda4e0143ba9f9590b59d421942c6c866c63c
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2020-09-19 08:14:20 -0400

    setup.py updated to a new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as r:
 
 setup(
     name='vizex',
-    version='1.1.0',
+    version='1.3.2',
     author='Beka Modebadze',
     author_email='bexxmodd@seas.upenn.edu',
     description='UNIX/Linux Terminal program to graphically display the disk space usage',
