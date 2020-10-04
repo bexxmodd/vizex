e1ae072c56ca27c85f2aa5048a814a313ca04e03
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2020-10-04 17:41:51 -0400

    started template for unit testing battery module

---
+++ tests/test_battery.py
@@ -0,0 +1,15 @@
+import unittest
+
+from battery import battery
+
+class TestBattery(unittest.TestCase):
+    """ Test battry module """
+
+    def test_print_charts(self):
+        pass
+
+    def test_print_battery_chart(self):
+        pass
+
+    def test_create_details(self):
+        pass
