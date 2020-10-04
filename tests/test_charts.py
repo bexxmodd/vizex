32b7b8ea0583ebf722f9c0a61137e83cb517efd3
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2020-10-04 13:50:27 -0400

    Build the test case for charts, only the vertical bar calss is left and need to correct test_chart

--- tests/test_charts.py
+++ tests/test_charts.py
@@ -1,40 +1,126 @@
+import io
 import unittest
-from tools import Color
-from colored import fg, stylize
-from charts import ChartPrint
+import unittest.mock
 
-class TestChartPrint(unittest.TestCase):
-    """Test Printing of Charts in the terminal"""
+from colored import fg, attr, stylize
+from charts import Options, Chart, HorizontalBarChart
+
+class TestOptions(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.chart01 = ChartPrint(graph=Color.RED)
-        cls.chart02 = ChartPrint(symbol='@')
+        cls.opts = Options()
+        # cls.opts.graph_color = 'cyan'
+        # cls.opts.header_style = 'underlined'
+        # cls.opts.text_color = 'yellow'
 
-    def test_grap_color(self):
-        self.assertIsInstance(self.chart01,
-                                ChartPrint)
-        self.assertIsInstance(self.chart02,
-                                ChartPrint)
-        self.assertEqual(self.chart01.graph,
-                                fg('red'))
-        self.assertEqual(self.chart01.msymbol,
-                            stylize('▒', fg('red')))
-        self.assertEqual(self.chart02.fsymbol, '@')
-        self.assertEqual(self.chart02.esymbol, '-')
+    def test_symbol_setter(self):
+        self.opts.symbol = '@'
+        self.assertEqual(self.opts.fsymbol, '@')
+        self.assertEqual(self.opts.msymbol, '>')
+        self.assertEqual(self.opts.esymbol, '-')
+
+    def test_check_color(self):
+        self.opts.graph_color = 'cyan'
+        self.assertEqual(
+            fg('cyan'), self.opts.graph_color)
+
+        self.assertEqual(
+            'white', self.opts._check_color('notcolor'))
+        self.assertEqual(
+            fg('magenta'), self.opts._check_color('magenta'))
+
+    def test_check_attr(self):
+        self.opts.header_style = 'underlined'
+        self.assertEqual(
+            attr('underlined'), self.opts.header_style)
+
+        self.assertEqual(
+            'bold', self.opts._check_attr('notattr'))
+        self.assertEqual(
+            attr('blink'), self.opts._check_attr('blink'))
+
+
+class TestChart(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.chart = Chart()
+
+    def test_chart_init(self):
+        # Test that if no Options arg given new is created
+        self.assertIsInstance(self.chart.options, Options)
+
+
+class TestHorizontalBarChart(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.horizontal_chart = HorizontalBarChart()
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def assert_stdout(self, expected_output, mock_stdout):
+        self.horizontal_chart.chart(
+            title='Test Title',
+            pre_graph_text='This looks sweet',
+            post_graph_text=None,
+            footer=None,
+            maximum=10,
+            current=5
+        )
+
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_chart(self):
+        compare = f"{stylize('Test Title', fg('red') + attr('bold'))}\n" \
+            + f"{stylize('This looks sweet', fg('white'))}\n" \
+            + f"{stylize('███████████████████▒░░░░░░░░░░░░░░░░░░░', fg('white'))}"
         
+        self.assert_stdout(compare)
+
     def test_draw_horizontal_bar(self):
-        compare = '[@@@@@@@@@@@@@@@@@@>------------------]'
+        compare = '███████████████████▒░░░░░░░░░░░░░░░░░░░'
         self.assertEqual(
-            self.chart02.draw_horizontal_bar(10, 5),
-            compare
+            compare, self.horizontal_chart.draw_horizontal_bar(10, 5))
+
+        compare_02 = '▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
+        self.assertEqual(
+            compare_02, self.horizontal_chart.draw_horizontal_bar(10, 0)
         )
 
-    def test_draw_vertical_bar(self):
-        self.assertEqual(
-            self.chart02.draw_vertical_bar(10, 5)[:22],
-            '\n---------  \n---------'
-        )
+
+# class TestChartPrint(unittest.TestCase):
+#     """Test Printing of Charts in the terminal"""
+
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.chart01 = ChartPrint(graph=Color.RED)
+#         cls.chart02 = ChartPrint(symbol='@')
+
+#     def test_grap_color(self):
+#         self.assertIsInstance(self.chart01,
+#                                 ChartPrint)
+#         self.assertIsInstance(self.chart02,
+#                                 ChartPrint)
+#         self.assertEqual(self.chart01.graph,
+#                                 fg('red'))
+#         self.assertEqual(self.chart01.msymbol,
+#                             stylize('▒', fg('red')))
+#         self.assertEqual(self.chart02.fsymbol, '@')
+#         self.assertEqual(self.chart02.esymbol, '-')
+        
+#     def test_draw_horizontal_bar(self):
+#         compare = '[@@@@@@@@@@@@@@@@@@>------------------]'
+#         self.assertEqual(
+#             self.chart02.draw_horizontal_bar(10, 5),
+#             compare
+#         )
+
+#     def test_draw_vertical_bar(self):
+#         self.assertEqual(
+#             self.chart02.draw_vertical_bar(10, 5)[:22],
+#             '\n---------  \n---------'
+#         )
 
 
 if __name__ == '__main__':
--- tests/test_disk.py
+++ tests/test_disk.py
@@ -7,142 +7,158 @@ from unittest.mock import MagicMock, call
 from colored import fg, attr, stylize
 from tools import Color, Attr, Chart
 from disks import DiskUsage
-from charts import ChartPrint
+from charts import Options
 
-
-class TestDiskUsage(unittest.TestCase):
-    """Test DiskUsage class"""
+class TestOptions:
 
     @classmethod
     def setUpClass(cls):
-        # Create mock up class for method testing
-        cls.du = DiskUsage(
-            chart=Chart.BARH,
-            path=None,
-            exclude=[],
-            header=Color.PINK,
-            style=Attr.UNDERLINED,
-            details=False,
-            every=False,
-            symbol=None,
-            text=Color.BEIGE,
-            graph=None
-        )
-        cls.test_disk = {
-            'total': 100000,
-            'used': 60000,
-            'free': 40000,
-            'percent': 60,
-            'fstype': 'ext4',
-            'mountpoint': '/test'
+        cls.opts = Options()
+        # cls.opts.graph_color = 'cyan'
+        # cls.opts.header_style = 'underlined'
+        # cls.opts.text_color = 'yellow'
 
-        }
-        cls.dict_to_test_raise = {
-                'total': 100,
-                'not_used': 50,
-                'not_free': 50,
-                'precent': 50,
-                'fstype': 'ext4',
-                'mountpoint': '/test'
-        }
+    def test_check_color(self):
+        self.opts.graph_color = 'cyan'
+        self.assertEqual(fg('cyan'), self.opts.graph_color)
+        
+        self.opts.text_color = 'notcolor'
+        self.assertEqual(fg('red'), self.opts.text_color)
+
+
+# class TestDiskUsage(unittest.TestCase):
+#     """Test DiskUsage class"""
+
+#     @classmethod
+#     def setUpClass(cls):
+#         # Create mock up class for method testing
+#         cls.du = DiskUsage(
+#             chart=Chart.BARH,
+#             path=None,
+#             exclude=[],
+#             header=Color.PINK,
+#             style=Attr.UNDERLINED,
+#             details=False,
+#             every=False,
+#             symbol=None,
+#             text=Color.BEIGE,
+#             graph=None
+#         )
+#         cls.test_disk = {
+#             'total': 100000,
+#             'used': 60000,
+#             'free': 40000,
+#             'percent': 60,
+#             'fstype': 'ext4',
+#             'mountpoint': '/test'
+
+#         }
+#         cls.dict_to_test_raise = {
+#                 'total': 100,
+#                 'not_used': 50,
+#                 'not_free': 50,
+#                 'precent': 50,
+#                 'fstype': 'ext4',
+#                 'mountpoint': '/test'
+#         }
     
-    def test_main(self):
-        self.assertIsInstance(self.du, DiskUsage,
-                    msg="Class doesn't instantiate properly")
-        self.du.switch = MagicMock()
-        self.du.main()
-        self.du.switch.assert_called()
+#     def test_main(self):
+#         self.assertIsInstance(self.du, DiskUsage,
+#                     msg="Class doesn't instantiate properly")
+#         self.du.switch = MagicMock()
+#         self.du.main()
+#         self.du.switch.assert_called()
 
-    def test_switch(self):
-        self.du = MagicMock()
-        self.du(1).switch().grab_partitions()(2.0).print_horizontal_barchart('test',
-                                                                        self.test_disk)
-        kall = call(1).switch().grab_partitions()(2.0).print_horizontal_barchart('test',
-                                                                        self.test_disk)
-        self.du.mock_calls == kall.call_list()
+#     def test_switch(self):
+#         self.du = MagicMock()
+#         self.du(1).switch().grab_partitions()(2.0).print_horizontal_barchart('test',
+#                                                                         self.test_disk)
+#         kall = call(1).switch().grab_partitions()(2.0).print_horizontal_barchart('test',
+#                                                                         self.test_disk)
+#         self.du.mock_calls == kall.call_list()
 
-    def test_grab_partitions(self):
-        disks = self.du.grab_partitions(self.du.exclude)
-        self.assertIsInstance(disks, dict,
-                msg='Function should return dict')
-        # Test that proper keys are present
-        compare_keys = ['total',
-                        'used',
-                        'free',
-                        'percent',
-                        'fstype',
-                        'mountpoint']
-        for disk in disks:
-            self.assertIsNotNone(disk)
-            keys = [key for key in disks[disk].keys()]
-            self.assertListEqual(keys, compare_keys,
-                        msg=f'{keys} are not present')
+#     def test_grab_partitions(self):
+#         disks = self.du.grab_partitions(self.du.exclude)
+#         self.assertIsInstance(disks, dict,
+#                 msg='Function should return dict')
+#         # Test that proper keys are present
+#         compare_keys = ['total',
+#                         'used',
+#                         'free',
+#                         'percent',
+#                         'fstype',
+#                         'mountpoint']
+#         for disk in disks:
+#             self.assertIsNotNone(disk)
+#             keys = [key for key in disks[disk].keys()]
+#             self.assertListEqual(keys, compare_keys,
+#                         msg=f'{keys} are not present')
         
-        # Test that they have positive integer values
-        for disk in disks:
-            self.assertGreaterEqual(disks[disk]['total'], 1)
-            self.assertGreaterEqual(disks[disk]['used'], 1)
-            self.assertGreaterEqual(disks[disk]['free'], 0)
-            self.assertGreaterEqual(disks[disk]['percent'], 0)
-            self.assertIsInstance(disks[disk]['fstype'], str)
-            self.assertIsInstance(disks[disk]['mountpoint'], str)
+#         # Test that they have positive integer values
+#         for disk in disks:
+#             self.assertGreaterEqual(disks[disk]['total'], 1)
+#             self.assertGreaterEqual(disks[disk]['used'], 1)
+#             self.assertGreaterEqual(disks[disk]['free'], 0)
+#             self.assertGreaterEqual(disks[disk]['percent'], 0)
+#             self.assertIsInstance(disks[disk]['fstype'], str)
+#             self.assertIsInstance(disks[disk]['mountpoint'], str)
 
-    def test_grab_specific_disk(self):
-        disks = self.du.grab_specific_disk('/home/')
-        self.assertIsInstance(disks, dict,
-                msg='Function should return dict')
-        # Test that proper keys are present
-        compare_keys = ['total',
-                        'used',
-                        'free',
-                        'percent',
-                        'fstype',
-                        'mountpoint']
-        for disk in disks:
-            self.assertIsNotNone(disks)
-            keys = [key for key in disks[disk].keys()]
-            self.assertListEqual(keys, compare_keys,
-                            msg=f'{keys} are not present')
+#     def test_grab_specific_disk(self):
+#         disks = self.du.grab_specific_disk('/home/')
+#         self.assertIsInstance(disks, dict,
+#                 msg='Function should return dict')
+#         # Test that proper keys are present
+#         compare_keys = ['total',
+#                         'used',
+#                         'free',
+#                         'percent',
+#                         'fstype',
+#                         'mountpoint']
+#         for disk in disks:
+#             self.assertIsNotNone(disks)
+#             keys = [key for key in disks[disk].keys()]
+#             self.assertListEqual(keys, compare_keys,
+#                             msg=f'{keys} are not present')
         
-        # Test that they have positive integer values
-        for disk in disks:
-            self.assertGreaterEqual(disks[disk]['total'], 1)
-            self.assertGreaterEqual(disks[disk]['used'], 1)
-            self.assertGreaterEqual(disks[disk]['free'], 0)
-            self.assertGreaterEqual(disks[disk]['percent'], 0)
-            self.assertIsInstance(disks[disk]['fstype'], str)
-            self.assertIsInstance(disks[disk]['mountpoint'], str)
+#         # Test that they have positive integer values
+#         for disk in disks:
+#             self.assertGreaterEqual(disks[disk]['total'], 1)
+#             self.assertGreaterEqual(disks[disk]['used'], 1)
+#             self.assertGreaterEqual(disks[disk]['free'], 0)
+#             self.assertGreaterEqual(disks[disk]['percent'], 0)
+#             self.assertIsInstance(disks[disk]['fstype'], str)
+#             self.assertIsInstance(disks[disk]['mountpoint'], str)
 
-    def test_print_horizontal_barchart(self):
-        self.du.create_stats = MagicMock()
-        self.du.print_horizontal_barchart(
-                                'test_disk', self.test_disk)
-        self.du.create_stats.assert_called()
+#     def test_print_horizontal_barchart(self):
+#         self.du.create_stats = MagicMock()
+#         self.du.print_horizontal_barchart(
+#                                 'test_disk', self.test_disk)
+#         self.du.create_stats.assert_called()
 
-    def test_color_details_text(self):
-        expected_output = stylize(f"fstype={self.test_disk['fstype']}\tmountpoint={self.test_disk['mountpoint']}",
-                        fg(self.du.text.value))
-        self.assertEqual(
-            expected_output, self.du.color_details_text(self.test_disk))
+#     def test_color_details_text(self):
+#         expected_output = stylize(f"fstype={self.test_disk['fstype']}\tmountpoint={self.test_disk['mountpoint']}",
+#                         fg(self.du.text.value))
+#         self.assertEqual(
+#             expected_output, self.du.color_details_text(self.test_disk))
 
-    def test_create_stats(self):
-        # Switch back text color to defualt
-        self.du.text = None
-        compare = "Total: 97.7 KB\t Used: 58.6 KB\t Free: 39.1 KB"
-        self.assertEqual(
-            self.du.create_stats(self.test_disk), compare)
+#     def test_create_stats(self):
+#         # Switch back text color to defualt
+#         self.du.text = None
+#         compare = "Total: 97.7 KB\t Used: 58.6 KB\t Free: 39.1 KB"
+#         self.assertEqual(
+#             self.du.create_stats(self.test_disk), compare)
         
-    def test_create_warning(self):
-        # Check warning when disk is at capacity
-        compare_high = stylize('80% full',
-            attr(Attr.BLINK.value) + fg(Color.LIGHT_RED.value))
-        self.assertEqual(self.du.create_warning(80), compare_high)
-        # Check when disk is half full
-        compare_med = stylize('60% full', fg(Color.ORANGE.value))
-        self.assertEqual(self.du.create_warning(60), compare_med)
-        # Check when disk is nearly empty
-        compare_low = stylize('10% full', fg(Color.NEON.value))
-        self.assertEqual(self.du.create_warning(10), compare_low)
+#     def test_create_warning(self):
+#         # Check warning when disk is at capacity
+#         compare_high = stylize('80% full',
+#             attr(Attr.BLINK.value) + fg(Color.LIGHT_RED.value))
+#         self.assertEqual(self.du.create_warning(80), compare_high)
+#         # Check when disk is half full
+#         compare_med = stylize('60% full', fg(Color.ORANGE.value))
+#         self.assertEqual(self.du.create_warning(60), compare_med)
+#         # Check when disk is nearly empty
+#         compare_low = stylize('10% full', fg(Color.NEON.value))
+#         self.assertEqual(self.du.create_warning(10), compare_low)
 
 
 if __name__ == '__main__':
